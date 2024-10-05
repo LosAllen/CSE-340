277d98b7c7b934fd16a5c7044d5cc45f115216c1
LosAllen <lincolnjallen@gmail.com>
2024-10-05 09:32:21 -0500

    test

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * view engine and templates
@@ -22,9 +23,8 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-app.get("/", function(req, res) {
-  res.render("index", {title:"Home"})
-})
+app.get("/", baseController.buildHome)
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
